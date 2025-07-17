b85ad5b1954df2ce3bee745a50847b90c1f10e25
Maxim Kryuchkov <7293121+observant-sun@users.noreply.github.com>
2025-07-17 18:33:43 +0300

    Change build system to Maven, use Spring Boot; move files in accord with Maven project structure

---
+++ src/main/java/ru/mkryuchkov/blogya/BlogyaApplication.java
@@ -0,0 +1,13 @@
+package ru.mkryuchkov.blogya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mkryuchkov/blogya/BlogyaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mkryuchkov.blogya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
